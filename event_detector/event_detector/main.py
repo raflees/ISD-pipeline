a3dd17479f9a02171d4b5cf5e9b22c49c9858fe5
unknown <rafles1895@gmail.com>
2024-06-30 22:08:13 -0300

    created main.py

---
+++ event_detector/event_detector/main.py
@@ -0,0 +1,15 @@
+from controllers import ISDController
+from entities import CloudStorageState, PubSubDispatcher, LocalState
+
+def run(pattern="A(.*).gz"):
+    project_id = 'cellular-retina-427804-i7'
+    
+    dispatcher = PubSubDispatcher(project_id, 'isd-pipeline-changed-files')
+    state = CloudStorageState(project_id, 'isd-pipeline-state', 'state.json')
+
+    controller = ISDController(
+        pattern=pattern,
+        state=state,
+        dispatcher=dispatcher)
+    controller.get_and_dispatch_changed_events()
+    controller.write_state()
