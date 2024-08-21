ccfc367cc91644330b9e62c185701dfafd082b20
Rafael <rafles1895@gmail.com>
2024-08-20 23:16:54 -0300

    random stuff

--- ingest/tests/test_pubsub_parser.py
+++ ingest/tests/test_pubsub_parser.py
@@ -25,6 +25,13 @@ def test_pull_messages(patch_get_client, patch_pull_messages):
         {"file_name": "file3.gz", "file_url": "https://test.com/file3.gz"},
     ]
 
+def test_parse_target_info_duplicated_messages(patch_get_client, patch_pull_duplicated_messages):
+    parser = PubSubParser({"pubsub": {"subscription": "test"}})
+    assert list(parser.parse_target_info()) == [
+        {"file_name": "file1.gz", "file_url": "https://test.com/file1.gz"},
+        {"file_name": "file2.gz", "file_url": "https://test.com/file2.gz"},
+    ]
+
 @pytest.fixture
 def patch_get_client(monkeypatch):
     monkeypatch.setattr(PubSubParser, "_get_client", MockPubSubClient)
@@ -44,4 +51,18 @@ def patch_pull_messages(monkeypatch):
         PubSubParser,
         "_pull_messages",
         lambda self: [mocked_message_1, mocked_message_2, empty_mocked_message]
+    )
+
+@pytest.fixture
+def patch_pull_duplicated_messages(monkeypatch):
+    mocked_message = MockPubSubMessage(
+        data='[\'{"file_name": "file1.gz", "url": "https://test.com/file1.gz", "last_modified": "2024-08-03 13:04:00"}, ' +
+        '{"file_name": "file1.gz", "url": "https://test.com/file1.gz", "last_modified": "2024-08-03 13:06:00"}, ' +
+        '{"file_name": "file2.gz", "url": "https://test.com/file2.gz", "last_modified": "2024-08-03 13:04:00"}\']'
+    )
+    
+    monkeypatch.setattr(
+        PubSubParser,
+        "_pull_messages",
+        lambda self: [mocked_message]
     )
