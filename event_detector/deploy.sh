aa357637fd75c4f0bde0ad73d783f0a863a0e992
Rafael <rafles1895@gmail.com>
2024-08-27 18:23:51 -0300

    add auth list command

--- event_detector/deploy.sh
+++ event_detector/deploy.sh
@@ -1,8 +1,9 @@
+gcloud auth list
+
 gcloud functions deploy event_detector_test \
     --gen2 \
     --region=us-central1 \
     --runtime=python312 \
     --memory=256MB \
-    --source=. \
     --entry-point=event_detector \
     --trigger-http --allow-unauthenticated
