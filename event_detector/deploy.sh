777f7cf9e20fb8a95ef2bf4623734f4acdfd4f3c
Rafael <rafles1895@gmail.com>
2024-08-27 19:42:48 -0300

    reverted to Actions

--- event_detector/deploy.sh
+++
@@ -1,9 +0,0 @@
-gcloud auth list
-
-gcloud functions deploy event_detector_test \
-    --gen2 \
-    --region=us-central1 \
-    --runtime=python312 \
-    --memory=256MB \
-    --entry-point=event_detector \
-    --trigger-http --allow-unauthenticated
