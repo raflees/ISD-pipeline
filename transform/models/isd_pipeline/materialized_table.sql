c85396317e0eac5eda9cbfab60747b37900505f2
Rafael <rafles1895@gmail.com>
2024-08-27 22:33:52 -0300

    unnested data

--- transform/models/isd_pipeline/materialized_table.sql
+++ transform/models/isd_pipeline/materialized_table.sql
@@ -4,5 +4,6 @@
 ) }}
 
 SELECT
-    *
+    JSON_VALUE(records, "$.data") raw_data,
+    CAST(ingest_timestamp AS TIMESTAMP) AS ingest_timestamp,
 FROM {{ source('isd_raw', 'raw_integrated_surface_data') }}
