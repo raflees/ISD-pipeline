cfe134ea4be57fee75b1a756dd690d1e18d0f7ef
Rafael <rafles1895@gmail.com>
2024-09-04 21:45:13 -0300

    started dockerfile

--- transform/models/isd_pipeline/isd_pipeline_unnested.sql
+++ transform/models/isd_pipeline/isd_pipeline_unnested.sql
@@ -6,4 +6,5 @@
 SELECT
     JSON_VALUE(records, "$.data") raw_data,
     CAST(ingest_timestamp AS TIMESTAMP) AS ingest_timestamp,
+    _FILE_NAME AS file_name,
 FROM {{ source('isd_raw', 'raw_integrated_surface_data') }}
