0490a02b86ced655f13b1e2bd7b68919357b6658
Rafael <rafles1895@gmail.com>
2024-09-17 21:10:05 -0300

    test with failed transform

--- transform/models/isd_pipeline/isd_pipeline_unnested.sql
+++ transform/models/isd_pipeline/isd_pipeline_unnested.sql
@@ -7,4 +7,4 @@ SELECT
     JSON_VALUE(records, "$.data") raw_data,
     CAST(ingest_timestamp AS TIMESTAMP) AS ingest_timestamp,
     _FILE_NAME AS file_name,
-FROM {{ source('isd_raw', 'raw_integrated_surface_data') }}
+FROM {{ source('isd_raw', 'i_dont_exist') }}
